3f50f0eb5df18a593b32a2985de2be8f3d376202
Siva Krishna <divyakola143@gmail.com>
2024-09-17 16:12:12 +0530

    Update settings.py

--- proj1/proj1/settings.py
+++ proj1/proj1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'liw9+m1vhf2gg#e#txb+fd4c_8(8@)&b!6f^y_ijzs#re-sq_d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
